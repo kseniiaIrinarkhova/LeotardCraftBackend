5aa3da62c7fd3a2693965cda14467ec2fb509089
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-03-20 21:28:28 -0700

    login user using hashed password in DB

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,19 +1,27 @@
 import { IUser } from '../types/main';
 import UserModel from '../models/User.model'
+import bcrypt from 'bcrypt';
 
 export async function register(user: IUser): Promise<void> {
     try {
+        //try to create a new user in database
         await UserModel.create(user);
     } catch (error) {
         throw error;
     }
 }
 
-export async function login(user: IUser) : Promise<IUser> {
+export async function login(user: IUser): Promise<IUser> {
     try {
-        const foundUser = await UserModel.findOne({ username: user.username, password: user.password });
-        console.log(foundUser)
-        if (foundUser) return foundUser;
+        //try to find requested username
+        const foundUser = await UserModel.findOne({ username: user.username });
+        //there is no such user in database
+        if (!foundUser) throw new Error("Invalid Credentials");
+        //compare requested password with hashed one in database
+        const isMatch = bcrypt.compareSync(user.password, foundUser.password);
+        //if match - return user
+        if (isMatch) return foundUser;
+        //else throw an error
         throw new Error("Invalid Credentials")
     } catch (error) {
         throw error;
