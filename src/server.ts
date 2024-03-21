3f810418541a0bf79c35d37b4d55a5121329d9fd
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-03-20 20:07:21 -0700

    added connection to MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,41 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.config";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const connectionString: string = process.env.ATLAS_URI || "";
+
+//Middleware
+//support for JSON data in the payload
+app.use(express.json());
+//parse incoming requests with urlencoded payloads and is based on body-parser
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+//Errors
+
+
+// connecting to Mongodb and starting the server
+const startDB = async () => {
+    try {
+        await connectDB(connectionString);
+        console.log('Mongodb is connected!!!')
+        app.listen(port, () => {
+            console.log(`[server]: Server is running at http://localhost:${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startDB();
+
+
