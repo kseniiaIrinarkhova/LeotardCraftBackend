28d73ab0582374ecaef398f1c51db96e472ebcf0
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-03-20 21:52:50 -0700

    return jws token as a result og login action

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,11 @@
 import { IUser } from '../types/main';
 import UserModel from '../models/User.model'
 import bcrypt from 'bcrypt';
+import jwt, { Secret } from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
+
+const jwtKey: Secret = process.env.JWT_TOKEN_KEY || 'Secret';
 
 export async function register(user: IUser): Promise<void> {
     try {
@@ -11,7 +16,7 @@ export async function register(user: IUser): Promise<void> {
     }
 }
 
-export async function login(user: IUser): Promise<IUser> {
+export async function login(user: IUser) {
     try {
         //try to find requested username
         const foundUser = await UserModel.findOne({ username: user.username });
@@ -19,8 +24,23 @@ export async function login(user: IUser): Promise<IUser> {
         if (!foundUser) throw new Error("Invalid Credentials");
         //compare requested password with hashed one in database
         const isMatch = bcrypt.compareSync(user.password, foundUser.password);
-        //if match - return user
-        if (isMatch) return foundUser;
+        //if match - return user info and token
+        if (isMatch) {
+            //create payload with user ID and username information
+            const payload = {
+                user: { 
+                    id: foundUser._id?.toString(),
+                    username: foundUser.username
+                 }
+            }
+            //create token that would expire in 2 days
+            const token = jwt.sign(payload, jwtKey, {
+                expiresIn: '2 days',
+            });
+
+            //return jws token with paylod
+            return { token: token };
+        }
         //else throw an error
         throw new Error("Invalid Credentials")
     } catch (error) {
